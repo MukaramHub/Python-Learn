ea53a0ba6ad6aac1a4f07006c583ed924ba1059e
maridegb <mukaramkosoko@gmail.com>
2026-02-28 15:27:46 +0100

    today's practice

--- app.py
+++ app.py
@@ -2,3 +2,20 @@ print("Hello World")
 
 age = 20
 print(age)
+
+price = 19.5
+first_name = "Mukaram"
+is_online = False
+
+print(price, first_name, is_online)
+
+
+input("What is your name? ")
+
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year)
+print(age)
